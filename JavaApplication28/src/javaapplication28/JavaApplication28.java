ec9729e9a465841366664734b3b4caf0a72b627b
trantiendat07102003 <trantiendatas@gmail.com>
2023-03-01 23:12:10 +0700

    three commit

---
+++ JavaApplication28/src/javaapplication28/JavaApplication28.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication28;
+
+/**
+ *
+ * @author trant
+ */
+public class JavaApplication28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello worl");
+        // TODO code application logic here
+    }
+    
+}
